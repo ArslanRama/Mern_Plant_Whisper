89a6e1ee9be98972da13976f5f5e4f4e6be9735a
ArslanRama <arslanrama84@gmail.com>
2021-05-18 09:57:42 +0200

    concurrently installed

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,5 @@ app.post('/user/data', (req, res)=> {
 })
 
 app.listen(5000, ()=>{
-    console.log('Backend is running on port 5000')
+    console.log('Backend is running on http://localhost:5000/')
 })

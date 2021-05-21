196e71a73c48b9bf7c1c0846d6431dfe804e2d13
ArslanRama <arslanrama84@gmail.com>
2021-05-21 11:43:13 +0200

    mongo db and insomnia practice

--- server/server.js
+++ server/server.js
@@ -25,6 +25,22 @@ app.use(cors());
 app.use(express.json())
 
 //! routes as REST API for frontend
+app.get('/user/data', (req, res) => {
+    res.json('test worj')
+})
+//! testing INSOMNIA
+app.get('/login', (req, res) => {
+    res.status(200).json({ msg: 'login page' })
+})
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+    if (username === 'Aslan' && password === '123456') {
+        return res.status(200).json({ msg: 'logged in successfully' })
+    }
+        res.json({ msg: 'wrong password' })
+
+})
+
 app.use('/plant', plantRouter);
 app.post('/user/data', (req, res) => {
     // some data from frontend react UI
@@ -34,8 +50,10 @@ app.post('/user/data', (req, res) => {
     res.json({
         msg: 'successfully received!',
         username: req.body.username,
+        email: req.body.email
     })
 })
+
 
 
 app.use('/contact', contactRouter);

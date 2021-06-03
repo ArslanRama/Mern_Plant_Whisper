596ea7d380383a749e37c0c3554bb86cc09d4e26
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-03 21:52:39 +0200

    edit the Schema and added bcrypt to hash the existing passwords with the new passwords

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      require: true,
+      required: true,
       min: 3,
       max: 20,
       unique: true,
@@ -20,15 +20,30 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
       min: 6,
-    },
-    addressSchema: {
-      type: Schema.Types.ObjectId,
-      ref: 'Address'
     }
-   
   },
-  
-
 )
+UserSchema.pre('save',function(next){
+  if(!this.isModified('password'))
+      return next();
+  bcrypt.hash(this.password,10,(err,passwordHash)=>{
+      if(err)
+          return next(err);
+      this.password = passwordHash;
+      next();
+  });
+});
+
+UserSchema.methods.comparePassword = function(password,cb){
+  bcrypt.compare(password,this.password,(err,isMatch)=>{
+      if(err)
+          return cb(err);
+      else{
+          if(!isMatch)
+              return cb(null,isMatch);
+          return cb(null,this);
+      }
+  });
+}
 
 module.exports = mongoose.model("User", UserSchema)

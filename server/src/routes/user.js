8b9d19beb2d95f9102cd9c5e03f902fd1452aad1
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-03 21:56:12 +0200

    added new routes and authorize the login, logout  and register , used the jsonwebtoken as well

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,35 +1,52 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const bcrypt= require('bcrypt')
-
-//! connectin Address Schema in User Model
-router.get('/all', (req, res) => {
-  User.findById((err, user) => {
-      console.log(user)
-      res.json(user)
-  }).populate('addressSchema')
-})
-
-
-//! sign up 
-router.post('/create', (req, res)=>{
-  // save a user
-  const newUser = new User(req.body);
-  newUser.save((err, doc)=>{
-      if(err) throw err.message;
-      res.json(doc)
-  })
-})
-
-//! sign in
-router.post('/signin', (req, res) =>{
-  const{email, password} = req.body;
-  User.find({email, password}, (err, data)=>{
-    res.json(data)
-  })
-})
+const passport = require('passport');
+const JWT = require('jsonwebtoken');
 
 
 
+router.post('/register',(req,res)=>{
+  const { username,email, password } = req.body;
+  User.findOne({username},(err,user)=>{
+      if(err)
+          res.status(500).json({message : {msgBody : "Oops !! Error happened", msgError: true}});
+      if(user)
+          res.status(400).json({message : {msgBody : "Username is already taken", msgError: true}});
+      else{
+          const newUser = new User({username,email, password});
+          newUser.save(err=>{
+              if(err)
+                  res.status(500).json({message : {msgBody : "Oops !! Error happened", msgError: true}});
+              else
+                  res.status(201).json({message : {msgBody : "Account successfully created", msgError: false}});
+          });
+      }
+  });
+});
+
+const signToken = userID =>{
+  return JWT.sign({
+      iss : "Ahmad",
+      sub : userID
+  },"Ahmad",{expiresIn : "1h"});
+}
+
+router.post('/login',passport.authenticate('local',{session : false}),(req,res)=>{
+  if(req.isAuthenticated()){
+     const {_id,username,email} = req.user;
+     const token = signToken(_id);
+
+    /** Important Cookies Security:
+     * httpOnly : clientSide cant apply js to the cookie to prevent (XSS)
+     * sameSite: protect from cross-site request forgery attacks */
+     res.cookie('access_token',token,{httpOnly: true, sameSite:true}); 
+     res.status(200).json({isAuthenticated : true,user : {username,email}});
+  }
+});
+
+router.get('/logout',passport.authenticate('jwt',{session : false}),(req,res)=>{
+  res.clearCookie('access_token');
+  res.json({user:{username : "",email: ""},success : true});
+});
 
 module.exports = router;

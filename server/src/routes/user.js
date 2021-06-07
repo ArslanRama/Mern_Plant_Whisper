20838316dc70e8b00588fee3766e084feb4d1b32
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-07 11:20:27 +0200

    added configPassport

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../models/User');
 const passport = require('passport');
 const JWT = require('jsonwebtoken');
+require('../config/passport')
 
 
 
